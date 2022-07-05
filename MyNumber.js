678fbed202d48f51ca8ca9fb29887532ccc1de6a
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-07-05 17:05:50 -0300

    go

---
+++ MyNumber.js
@@ -0,0 +1,33 @@
+/**
+ * Power you rasied power.
+ *
+ * @param {number} a Power asign now.
+ * @param {number} b The power, must be a natural number.
+ * @return {number} Create your a, b your own..
+ */
+ function power(a, b) {
+    return Math.pow(a, b)
+  }
+
+/**
+ * Mulitipler a x b (somehow?)
+ * 
+ * @param {number} a
+ * @param {number} b
+ * @param {string} name Change it name are fix.
+ * @return {number}
+ */
+function Mulitipler(a, b, name) {
+    return a * b;
+}
+
+/**
+ * Divide of Resources.
+ * 
+ * @param {number} a
+ * @param {number} b
+ * @return {number} I Think so what divide.
+ */
+function Divide(a, b) {
+    return a / b
+}
